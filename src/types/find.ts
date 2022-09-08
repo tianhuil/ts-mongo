6eae30d8a592c6f5a63d1f7027e61d0845e6bc0c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-09-08 15:02:15 -0400

    Update TsFindCursor.sort in ts-mongo

--- src/types/find.ts
+++ src/types/find.ts
@@ -7,7 +7,7 @@ import {
   FindOptions,
 } from 'mongodb'
 import { Projection } from './projection'
-import { Sort } from './sort'
+import { Sort, SortDirection } from './sort'
 import { RemodelType } from './util'
 
 export declare type RemodeledOptions<TSchema extends Document> = {
@@ -15,14 +15,20 @@ export declare type RemodeledOptions<TSchema extends Document> = {
   projection?: Projection<TSchema>
 }
 
-export declare type TsFindOneAndDeleteOptions<TSchema extends Document> =
-  RemodelType<RemodeledOptions<TSchema>, FindOneAndDeleteOptions>
+export declare type TsFindOneAndDeleteOptions<TSchema extends Document> = RemodelType<
+  RemodeledOptions<TSchema>,
+  FindOneAndDeleteOptions
+>
 
-export declare type TsFindOneAndReplaceOptions<TSchema extends Document> =
-  RemodelType<RemodeledOptions<TSchema>, FindOneAndReplaceOptions>
+export declare type TsFindOneAndReplaceOptions<TSchema extends Document> = RemodelType<
+  RemodeledOptions<TSchema>,
+  FindOneAndReplaceOptions
+>
 
-export declare type TsFindOneAndUpdateOptions<TSchema extends Document> =
-  RemodelType<RemodeledOptions<TSchema>, FindOneAndUpdateOptions>
+export declare type TsFindOneAndUpdateOptions<TSchema extends Document> = RemodelType<
+  RemodeledOptions<TSchema>,
+  FindOneAndUpdateOptions
+>
 
 export declare type TsFindOptions<TSchema extends Document> = RemodelType<
   RemodeledOptions<TSchema>,
@@ -34,7 +40,7 @@ export declare type TsFindCursor<TSchema extends Document> = RemodelType<
     clone(): TsFindCursor<TSchema>
     map<T extends Document>(transform: (doc: TSchema) => T): TsFindCursor<T>
     project<T extends Document>(value: Projection<TSchema>): TsFindCursor<T>
-    sort(sort: Sort<TSchema>): TsFindCursor<TSchema>
+    sort(sort: Sort<TSchema> | string, direction?: SortDirection): TsFindCursor<TSchema>
   },
   FindCursor
 >
