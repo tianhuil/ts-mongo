a13945a808ba42075f5a712062a49821dd8be3cd
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-09-17 21:31:40 -0400

    Tweak names and better document

--- src/types/flatten.ts
+++ src/types/flatten.ts
@@ -2,7 +2,7 @@ import { WithId } from 'mongodb'
 import { BaseTypes, NonArrayObject } from './util'
 
 export declare type FlattenFilterPaths<Type, IndexType extends number = 0> = Join<
-  NestedPaths<WithId<Type>, IndexType>,
+  FlattenPaths<WithId<Type>, IndexType>,
   '.'
 >
 
@@ -11,17 +11,17 @@ export declare type FlattenFilterPaths<Type, IndexType extends number = 0> = Joi
 type UpdateArrayHolder = '$' | '$[]'
 
 export declare type FlattenUpdatePaths<Type> = Join<
-  NestedPaths<WithId<Type>, UpdateArrayHolder>,
+  FlattenPaths<WithId<Type>, UpdateArrayHolder>,
   '.'
 >
 
-export declare type FlattenFilterType<TSchema, Property extends string> = _FlattenFilterType<
+export declare type FlattenFilterType<TSchema, Property extends string> = FlattenType<
   WithId<TSchema>,
   Property,
   `${number}`
 >
 
-export declare type FlattenUpdateType<TSchema, Property extends string> = _FlattenFilterType<
+export declare type FlattenUpdateType<TSchema, Property extends string> = FlattenType<
   WithId<TSchema>,
   Property,
   UpdateArrayHolder
@@ -35,25 +35,33 @@ declare type Join<T extends unknown[], D extends string> = T extends []
   ? `${T[0]}${D}${Join<R, D>}`
   : string
 
-declare type NestedPaths<Type, ArrayIndexType> = Type extends BaseTypes
+/**
+ * "dot path" of Type
+ */
+declare type FlattenPaths<Type, ArrayIndexType> = Type extends BaseTypes
   ? []
   : Type extends ReadonlyArray<infer ArrayType>
   ? ArrayType extends NonArrayObject
     ? // Can omit index for array of objects
       // https://docs.mongodb.com/manual/tutorial/query-array-of-documents/#combination-of-elements-satisfies-the-criteria
-      | NestedPaths<ArrayType, ArrayIndexType>
-        | [ArrayIndexType, ...NestedPaths<ArrayType, ArrayIndexType>]
+      | FlattenPaths<ArrayType, ArrayIndexType>
+        | [ArrayIndexType, ...FlattenPaths<ArrayType, ArrayIndexType>]
         | [ArrayIndexType] // Can stop at array
-    : [ArrayIndexType, ...NestedPaths<ArrayType, ArrayIndexType>] | [ArrayIndexType] // Can stop at array
+    : [ArrayIndexType, ...FlattenPaths<ArrayType, ArrayIndexType>] | [ArrayIndexType] // Can stop at array
   : Type extends Map<string, unknown>
   ? [string]
   : Type extends object
   ? {
-      [Key in Extract<keyof Type, string>]: [Key, ...NestedPaths<Type[Key], ArrayIndexType>] | [Key]
+      [Key in Extract<keyof Type, string>]:
+        | [Key, ...FlattenPaths<Type[Key], ArrayIndexType>]
+        | [Key]
     }[Extract<keyof Type, string>]
   : []
 
-declare type _FlattenFilterType<
+/**
+ * Given a dot path Property, give TSchema's type
+ */
+declare type FlattenType<
   TSchema,
   Property extends string,
   ArrayHolder extends string
@@ -72,13 +80,13 @@ declare type _FlattenFilterType<
   : Property extends `${infer Key}.${infer Rest}` // Compound key
   ? Key extends ArrayHolder
     ? TSchema extends ReadonlyArray<infer ArrayType>
-      ? _FlattenFilterType<ArrayType, Rest, ArrayHolder>
+      ? FlattenType<ArrayType, Rest, ArrayHolder>
       : never
     : Key extends keyof TSchema
-    ? _FlattenFilterType<TSchema[Key], Rest, ArrayHolder>
+    ? FlattenType<TSchema[Key], Rest, ArrayHolder>
     : never
   : TSchema extends ReadonlyArray<infer ArrayType> // Can omit index for array of objects
   ? ArrayType extends NonArrayObject
-    ? _FlattenFilterType<ArrayType, Property, ArrayHolder>
+    ? FlattenType<ArrayType, Property, ArrayHolder>
     : never
   : never
