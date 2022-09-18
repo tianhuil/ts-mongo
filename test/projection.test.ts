af77414b4b23e239c616766b75f870be347f0cd6
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-09-18 17:39:35 -0400

    Add projection test

---
+++ test/projection.test.ts
@@ -0,0 +1,41 @@
+import z from 'zod'
+import { mkTsTestCollection } from './util'
+
+const Example = z.object({
+  a: z.object({ b: z.number(), c: z.string() }),
+  d: z.boolean(),
+})
+type Example = z.infer<typeof Example>
+
+test('exclude with notation', async () => {
+  const col = await mkTsTestCollection<Example>()
+
+  await col.insertOne({ a: { b: 2, c: 's' }, d: true })
+  const result = await col.findOne({}, { projection: { 'a.b': 0 } })
+
+  delete (result as any)._id
+
+  expect(result).toEqual({ a: { c: 's' }, d: true })
+})
+
+test('exclude with dot notation', async () => {
+  const col = await mkTsTestCollection<Example>()
+
+  await col.insertOne({ a: { b: 2, c: 's' }, d: true })
+  const result = await col.findOne({}, { projection: { 'a.b': 0, 'a.c': 0 } })
+
+  delete (result as any)._id
+
+  expect(result).toEqual({ a: {}, d: true })
+})
+
+test('exclude without dot notation', async () => {
+  const col = await mkTsTestCollection<Example>()
+
+  await col.insertOne({ a: { b: 2, c: 's' }, d: true })
+  const result = await col.findOne({}, { projection: { d: 0 } })
+
+  delete (result as any)._id
+
+  expect(result).toEqual({ a: { b: 2, c: 's' } })
+})
