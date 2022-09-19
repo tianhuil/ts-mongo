6e2f3e7e27582982a19cb860fd1c8da00758e10a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-09-19 16:28:21 -0400

    Fix type error

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,5 +1,7 @@
-import { ModifyResult } from 'mongodb'
 import { DocumentWithId } from './util'
 
-export declare interface TsModifyResult<TSchema extends DocumentWithId>
-  extends ModifyResult<TSchema> {}
+export declare interface TsModifyResult<TSchema extends DocumentWithId> {
+  value: TSchema | null
+  lastErrorObject?: Document
+  ok: 0 | 1
+}
