77c0c7178e0a710f03cfcf48743b8cf513ad9ff9
Ryel Banfield <ryelbanfield@gmail.com>
2023-02-22 13:43:20 -0400

    Add $group to Pipeline

--- src/types/aggregation.ts
+++ src/types/aggregation.ts
@@ -1,9 +1,9 @@
-import { AggregationCursor, Document } from 'mongodb'
-import { TsFilter } from './filter'
-import { FlattenFilterPaths } from './flatten'
-import { TsProjection } from './projection'
-import { TsSort } from './sort'
-import { RemodelType } from './util'
+import { AggregationCursor, Document } from "mongodb";
+import { TsFilter } from "./filter";
+import { FlattenFilterPaths } from "./flatten";
+import { TsProjection } from "./projection";
+import { TsSort } from "./sort";
+import { RemodelType } from "./util";
 
 /**
  * This is an incomplete list but will do for now
@@ -13,18 +13,24 @@ export declare type Pipeline<TSchema extends Document, TSchemaOther extends Docu
   | { $project: TsProjection<TSchema> }
   | { $sort: TsSort<TSchema> }
   | { $lookup: TsLookup<TSchema, TSchemaOther> }
+  | { $group: TsGroup<TSchema> };
 
 export declare type TsLookup<TSchema extends Document, TSchemaOther extends Document> = {
-  from: string
-  localField: FlattenFilterPaths<TSchema>
-  foreignField: FlattenFilterPaths<TSchemaOther>
-  as: string
-}
+  from: string;
+  localField: FlattenFilterPaths<TSchema>;
+  foreignField: FlattenFilterPaths<TSchemaOther>;
+  as: string;
+};
 
 export declare type TsAggregationCursor<TSchema extends Document> = RemodelType<
   {
-    sort?: TsSort<TSchema>
-    projection?: TsProjection<TSchema>
+    sort?: TsSort<TSchema>;
+    projection?: TsProjection<TSchema>;
   },
   AggregationCursor<TSchema>
->
+>;
+
+export declare type TsGroup<TSchema extends Document> = {
+  _id: FlattenFilterPaths<TSchema> | { [key: string]: unknown };
+  [key: string]: unknown;
+};
