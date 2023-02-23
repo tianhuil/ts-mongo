f62abe8429377e80a794e631edf041675f3451f8
Herbert Kavuma <you@example.com>
2023-02-23 16:53:59 +0300

    Refactor

--- src/types/aggregation.ts
+++ src/types/aggregation.ts
@@ -1,4 +1,4 @@
-import { AggregationCursor, CollStats, Document } from 'mongodb'
+import { AggregationCursor, Document } from 'mongodb'
 import type { Collstats } from './colstats'
 import { TsFilter } from './filter'
 import { FlattenFilterPaths } from './flatten'
@@ -14,21 +14,25 @@ export declare type Pipeline<TSchema extends Document, TSchemaOther extends Docu
   | { $project: TsProjection<TSchema> }
   | { $sort: TsSort<TSchema> }
   | { $lookup: TsLookup<TSchema, TSchemaOther> }
-  /**
+  | {
+   /**
    * Limits the number of documents passed to the next stage in the pipeline and takes a positive integer that specifies the maximum number of documents to pass along.
    * 
    * Starting in MongoDB 5.0, the $limit pipeline aggregation has a 64-bit integer limit. Values passed to the pipeline which exceed this limit will return a invalid argument error.
    * 
    * https://www.mongodb.com/docs/manual/reference/operator/aggregation/limit/#mongodb-pipeline-pipe.-limit
-   */
-  | { $limit: Field }
-  /**
+   */ 
+    $limit: Field 
+  }
+  | {
+   /**
    * Returns statistics regarding a collection or view.
    * 
    * $collStats must be the first stage in an aggregation pipeline, or else the pipeline returns an error and it's not allowed in transactions.
    * https://www.mongodb.com/docs/manual/reference/operator/aggregation/collStats/#mongodb-pipeline-pipe.-collStats
-   */
-  | { $collStats: Collstats }
+   */ 
+    $collStats: Collstats 
+  }
 
 export declare type TsLookup<TSchema extends Document, TSchemaOther extends Document> = {
   from: string
