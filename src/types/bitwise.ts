11683b6aa4ce625fa192db7ec3d3442b6a042bcd
Michael Li <tianhui.michael.li@gmail.com>
2023-02-27 09:57:26 -0500

    Update bitwise.ts

--- src/types/bitwise.ts
+++ src/types/bitwise.ts
@@ -1,7 +1,8 @@
 import {Binary} from "mongodb";
 
 /**
- * https://www.mongodb.com/docs/manual/reference/operator/query-bitwise/
+ * https://www.mongodb.com/docs/manual/reference/operator/query-bitwise/ and
+ * https://mongodb.github.io/node-mongodb-native/5.1/modules.html#BitwiseFilter
  */
 export type WithBitwiseOperator<Field> = Field extends number | Binary | ReadonlyArray<number> ? {
     $bitsAllClear?: Field;
