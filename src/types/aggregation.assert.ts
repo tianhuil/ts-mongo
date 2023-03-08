f322377c65bc7ca3f9ddacac25d8e8543cd2f972
Daniel Montes de Oca <danielmdob@users.noreply.github.com>
2023-03-08 08:35:14 -0600

    add assertions for TsLookup (#4)
    * Add 2 assertions for TsLookup
    
    * assertions for $match, $project, $sort, $lookup
    
    * integrate prettier

--- src/types/aggregation.assert.ts
+++ src/types/aggregation.assert.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as ta from "type-assertions";
 import { Pipeline, TsLookup } from "./aggregation";
 
@@ -43,3 +44,130 @@ ta.assert<ta.Not<ta.Extends<{ $lookup: IncorrectLookupExample }, Pipeline<Exampl
 // Test Pipeline $group
 ta.assert<ta.Extends<{ $group: { _id: { a: number } } }, Pipeline<ExampleTSchema, ExampleTSchemaOther>>>();
 ta.assert<ta.Not<ta.Extends<{ $group: { _id: string } }, Pipeline<ExampleTSchema, ExampleTSchemaOther>>>>();
+=======
+import * as ta from 'type-assertions'
+import { TsLookup, Pipeline } from './aggregation'
+
+type ExampleTSchema = {
+  a: number[]
+  b: string
+  c: number
+}
+
+type ExampleTSchemaOther = {
+  x: number
+}
+
+type CorrectLookupExample = {
+  from: ''
+  localField: 'a'
+  foreignField: 'x'
+  as: ''
+}
+type IncorrectLookupExample = {
+  from: ''
+  localField: 'a'
+  foreignField: 'd'
+  as: ''
+}
+
+// Test TsLookup equivalence
+ta.assert<
+  ta.Extends<
+    CorrectLookupExample,
+    TsLookup<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      IncorrectLookupExample,
+      TsLookup<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+
+// Test Pipeline $match
+ta.assert<
+  ta.Extends<
+    { $match: { a: [1, 2] } },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      { $match: { a: '' } },
+      Pipeline<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      { $match: { x: [1, 2] } },
+      Pipeline<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+
+// Test Pipeline $project
+ta.assert<
+  ta.Extends<
+    { $project: { a: 1 } },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Extends<
+    { $project: { b: 1 } },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      { $project: { a: '1' } },
+      Pipeline<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      { $project: { x: 1 } },
+      Pipeline<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+
+// Test Pipeline $sort
+ta.assert<
+  ta.Extends<
+    { $sort: { c: -1 } },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Extends<
+    { $sort: { b: -1 } },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+
+// Test Pipeline $lookup
+ta.assert<
+  ta.Extends<
+    { $lookup: CorrectLookupExample },
+    Pipeline<ExampleTSchema, ExampleTSchemaOther>
+  >
+>()
+ta.assert<
+  ta.Not<
+    ta.Extends<
+      { $lookup: IncorrectLookupExample },
+      Pipeline<ExampleTSchema, ExampleTSchemaOther>
+    >
+  >
+>()
+>>>>>>> 536d300 (add assertions for TsLookup (#4))
