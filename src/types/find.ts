e3bfce95bf224dfbc3cb3d83ad6e840bf3e22768
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-08-01 16:08:49 -0400

    Fix issue

--- src/types/find.ts
+++ src/types/find.ts
@@ -1,1 +1,41 @@
-Twe
+import {
+  Document,
+  FindCursor,
+  FindOneAndDeleteOptions,
+  FindOneAndReplaceOptions,
+  FindOneAndUpdateOptions,
+  FindOptions,
+} from 'mongodb'
+import { TsProjection } from './projection'
+import { SortDirection, TsSort } from './sort'
+import { RemodelType, TimeOptions } from './util'
+
+// Remove sort and projection from options; instead use project / sort on the cursor
+// This simplifies return types
+export declare type TsFindOneAndDeleteOptions = Omit<
+  FindOneAndDeleteOptions,
+  'sort' | 'projection'
+>
+
+export declare type TsFindOneAndReplaceOptions = Omit<
+  FindOneAndReplaceOptions,
+  'sort' | 'projection'
+>
+
+export declare type TsFindOneAndUpdateOptions = TimeOptions &
+  Omit<FindOneAndUpdateOptions, 'sort' | 'projection'>
+
+export declare type TsFindOptions = Omit<FindOptions, 'sort' | 'projection'>
+
+export declare type TsFindCursor<TSchema extends Document> = RemodelType<
+  {
+    clone(): TsFindCursor<TSchema>
+    map<T extends Document>(transform: (doc: TSchema) => T): TsFindCursor<T>
+    project<T extends Document>(value: TsProjection<TSchema>): TsFindCursor<T>
+    sort(
+      sort: TsSort<TSchema> | string,
+      direction?: SortDirection
+    ): TsFindCursor<TSchema>
+  },
+  FindCursor<TSchema>
+>
