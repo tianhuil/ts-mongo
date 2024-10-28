71f4c51f8ab90ee1dea9779740ff28d60f8ad32e
Cauã Rinaldi <cauarinaldi451@gmail.com>
2024-10-28 15:26:51 -0300

    Move tests to separate file

---
+++ src/colection.distinct.assert.ts
@@ -0,0 +1,32 @@
+import type { ObjectId } from 'mongodb'
+import { TsReadWriteCollection } from './collection'
+import * as ta from 'type-assertions'
+
+type DistinctSampleType = {
+  _id: ObjectId
+  str: string
+  nested: {
+    field: number
+  }
+}
+
+type TestCollection = TsReadWriteCollection<
+  DistinctSampleType & { writeOnly: boolean },
+  DistinctSampleType & { readOnly: boolean }
+>
+
+const distinctString = (c: TestCollection) => c.distinct('str', {})
+const distinctId = (c: TestCollection) => c.distinct('_id', {})
+const distinctNested = (c: TestCollection) => c.distinct('nested.field', {})
+const distinctReadOnly = (c: TestCollection) => c.distinct('readOnly', {})
+
+ta.assert<ta.Equal<Awaited<ReturnType<typeof distinctString>>, string[]>>()
+ta.assert<ta.Equal<Awaited<ReturnType<typeof distinctId>>, ObjectId[]>>()
+ta.assert<ta.Equal<Awaited<ReturnType<typeof distinctNested>>, number[]>>()
+ta.assert<ta.Equal<Awaited<ReturnType<typeof distinctReadOnly>>, boolean[]>>()
+
+// @ts-expect-error
+const _distinctIncorrect = (c: TestCollection) => c.distinct('no', {})
+
+// @ts-expect-error
+const _distinctWriteOnly = (c: TestCollection) => c.distinct('writeOnly', {})
